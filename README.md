75b00cac55b8dcefd3551dcf76762ddd3a0623b2
devzinapsia <dev@zinapsia.com>
2026-02-18 08:44:36 -0300

    Initial structure for 18.0: Subscription Quotas

---
+++ README.md
@@ -0,0 +1,7 @@
+# Subscription Quotas Enhancement
+Este módulo extiende la funcionalidad nativa de Suscripciones de Odoo para permitir el control de cuotas numeradas.
+
+## Características:
+- Definición de cantidad máxima de cuotas.
+- Seguimiento de la próxima cuota a facturar.
+- Leyenda dinámica en las líneas de factura: "Cuota X de Y".
---
+++ suscription_quotas/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Subscription Quotas',
+    'version': '18.0.1.0.0', # Cambiar a 19.0 en la rama correspondiente
+    'category': 'Sales/Subscriptions',
+    'summary': 'Manejo de cuotas máximas y leyendas en facturación recurrente',
+    'author': 'Zinapsia',
+    'website': 'https://github.com/devzinapsia/suscriptions',
+    'depends': ['sale_subscription'],
+    'data': [
+        'views/sale_subscription_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'license': 'LGPL-3',
+}
